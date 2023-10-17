3f65cad8892ce3e19cab358c4ea3e536387eccce
Ayush Kumar Mishra <mishraayushsec@gmail.com>
2023-10-17 13:01:34 +0530

    preloader path issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import Footer from "./components/Footer/Footer";
 import HeaderNavbar from "./components/Navbar/HeaderNavbar";
-import Preloader from "./components/Preloader/preloader";
+import Preloader from "./components/Preloader/Preloader";
 import Cursor from "./components/Cursor/CustomCursor";
 import CommonNavbar from "./components/Navbar/CommonNavbar";
 import { Outlet } from "react-router-dom";
